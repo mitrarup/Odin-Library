4c25c04afe5d8f6f0b790f67e35a7738bc4c5bd8
Arup-mitra <arupmitra141@gmail.com>
2026-01-11 22:04:16 +0530

    Added Remove button...it is Working perfectly.

--- script.js
+++ script.js
@@ -13,37 +13,41 @@
 // const book1= new Book('The Habbit','J.R.R. Tolkien',295,'not read yet');
 // console.log(book1.info());
 
+runAgain();
+const myLibrary = [];
 
-const myLibrary=[];
-
-function Book(title,author,pages,status) {
-    if(!new.target){
+function Book(title, author, pages, status) {
+    if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.status=status;
-    this.id=crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.id = crypto.randomUUID();
 }
-let n=1;
+let n = 1;
 let container = document.querySelector(".card_container");
 
 
-function addBookToLibrary(title,author,pages,status) {
-  // take params, create a book then store it in the array
-    const book = new Book(title,author,pages,status);
-    
+function addBookToLibrary(title, author, pages, status) {
+    // take params, create a book then store it in the array
+    const book = new Book(title, author, pages, status);
+
     myLibrary.push(book);
     // myLibrary.push(new Book(title,author,pages,status));
     //NOw adding the book on page
-    let card=document.createElement("div");
+    let card = document.createElement("div");
     card.classList.add("card");
-    card.id=book.id;
-    card.innerHTML=`
+    card.id = book.id;
+    card.innerHTML = `
         <h1>${title}</h1>
         <h2>${author}</h2>
         <h2>${pages} Pages</h2>
+        <div>
+            <button class="button read_btn">Read</button>
+            <button class="button remove_btn" data-cardid="${book.id}">Remove</button>
+        </div>
     `;
     container.append(card);
 }
@@ -52,31 +56,46 @@ function addBookToLibrary(title,author,pages,status) {
 // console.log(myLibrary[0]);
 // console.log(myLibrary[1]);
 //Actual DOM
-const addBook=document.getElementById("add_book");
-const cancel=document.getElementById("cancel");
-const dialog=document.getElementById("my_dialog");
-const myForm=document.getElementById("myform");
+const addBook = document.getElementById("add_book");
+const cancel = document.getElementById("cancel");
+const dialog = document.getElementById("my_dialog");
+const myForm = document.getElementById("myform");
 
 
-addBook.addEventListener("click",()=>{
+addBook.addEventListener("click", () => {
     dialog.showModal();
 })
-cancel.addEventListener("click",()=>{
+cancel.addEventListener("click", () => {
     dialog.close();
 })
 
-myForm.addEventListener("submit", function(event){
+myForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const title=myForm.querySelector('[name="title"]').value;
-    const author=myForm.querySelector('[name="author"]').value;
-    const pages=myForm.querySelector('[name="pages"]').value;
-    const read=myForm.querySelector('[name="read"]').checked;
+    const title = myForm.querySelector('[name="title"]').value;
+    const author = myForm.querySelector('[name="author"]').value;
+    const pages = myForm.querySelector('[name="pages"]').value;
+    const read = myForm.querySelector('[name="read"]').checked;
     // console.log(title);
     // console.log(author);
     // console.log(pages);
     // console.log(read);
-    addBookToLibrary(title,author,pages,read);
+    addBookToLibrary(title, author, pages, read);
     dialog.close();
     myForm.reset();
+    runAgain();
 });
+
+// logic to remove the book
+function runAgain() {
+    const remove = document.querySelectorAll(".remove_btn");
+
+    remove.forEach(function (currentButton) {
+
+        currentButton.addEventListener("click", () => {
+            let cId = currentButton.dataset.cardid;
+            let remove_card = document.getElementById(cId);
+            remove_card.remove();
+        });
+    })
+}
